019ab88368427b8f139150b82f38275a26675060
Volodya-46 <volodya@noblescripts.com>
2024-01-25 12:46:43 +0400

    update

--- src/interfaces/global.ts
+++ src/interfaces/global.ts
@@ -1,6 +1,14 @@
+import {AxiosRequestConfig} from "axios";
+
 export interface IFormField {
     id: string
     name: string
     label: string
     type: string
+}
+
+export interface IRequestConfig extends AxiosRequestConfig {
+    headers?: {
+        Authorization: string
+    }
 }
--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,6 +1,9 @@
 import {isAuth} from "../helpers/isAuth";
-import {useEffect} from "react";
+import {useEffect, useState} from "react";
 import {useNavigate} from "react-router-dom";
+import {IUser} from "../interfaces/user";
+import axios from "axios";
+import {IRequestConfig} from "../interfaces/global";
 
 export default function Profile() {
     const navigate = useNavigate()
@@ -11,6 +14,20 @@ export default function Profile() {
             navigate('/')
         }
     }, [isAuthenticated, navigate]);
+
+    const [user, setUser] = useState<IUser | null>(null)
+
+    useEffect(() => {
+        axios.get(`${process.env.REACT_APP_SERVER_API_URI}/auth/me`, {
+            headers: {
+                Authorization: `Bearer ${localStorage.getItem(process.env.REACT_APP_AUTH_JWT as string)}`
+            }
+        } as IRequestConfig)
+            .then(res => {
+                setUser(res.data)
+            })
+            .catch(e => console.log(e))
+    }, []);
 
     return (
       <>
