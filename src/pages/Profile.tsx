821e0618e8eb519d1e708734c5abaf045d07a6e4
Volodya-46 <volodya@noblescripts.com>
2024-01-25 12:55:11 +0400

    update

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,9 +1,6 @@
 import {isAuth} from "../helpers/isAuth";
-import {useEffect, useState} from "react";
+import {useEffect} from "react";
 import {useNavigate} from "react-router-dom";
-import {IUser} from "../interfaces/user";
-import axios from "axios";
-import {IRequestConfig} from "../interfaces/global";
 
 export default function Profile() {
     const navigate = useNavigate()
@@ -15,23 +12,9 @@ export default function Profile() {
         }
     }, [isAuthenticated, navigate]);
 
-    const [user, setUser] = useState<IUser | null>(null)
-
-    useEffect(() => {
-        axios.get(`${process.env.REACT_APP_SERVER_API_URI}/auth/me`, {
-            headers: {
-                Authorization: `Bearer ${localStorage.getItem(process.env.REACT_APP_AUTH_JWT as string)}`
-            }
-        } as IRequestConfig)
-            .then(res => {
-                setUser(res.data)
-            })
-            .catch(e => console.log(e))
-    }, []);
-
     return (
       <>
-          <h1>profile page</h1>
+          <h1>profile page user</h1>
       </>
     )
 }
